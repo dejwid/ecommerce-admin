43d4914eab3cabc61fbd690b5b0cdd6f658972b8
dejwid <dawid.paszko@gmail.com>
2023-04-13 18:46:54 +0200

    adds secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,7 @@ import clientPromise from "@/lib/mongodb";
 const adminEmails = ['dawid.paszko@gmail.com'];
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
