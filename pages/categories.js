7bd2f4ec74c0a66a3f47e63cdbc530c421667360
Dawid Paszko <dawid.paszko@gmail.com>
2023-04-13 18:27:45 +0200

    missing keys in categories.js

--- pages/categories.js
+++ pages/categories.js
@@ -113,7 +113,7 @@ function Categories({swal}) {
                   value={parentCategory}>
             <option value="">No parent category</option>
             {categories.length > 0 && categories.map(category => (
-              <option value={category._id}>{category.name}</option>
+              <option key={category._id} value={category._id}>{category.name}</option>
             ))}
           </select>
         </div>
@@ -126,7 +126,7 @@ function Categories({swal}) {
             Add new property
           </button>
           {properties.length > 0 && properties.map((property,index) => (
-            <div className="flex gap-1 mb-2">
+            <div key={property.name} className="flex gap-1 mb-2">
               <input type="text"
                      value={property.name}
                      className="mb-0"
@@ -179,7 +179,7 @@ function Categories({swal}) {
           </thead>
           <tbody>
           {categories.length > 0 && categories.map(category => (
-            <tr>
+            <tr key={category._id}>
               <td>{category.name}</td>
               <td>{category?.parent?.name}</td>
               <td>
