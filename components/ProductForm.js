5bcda2c852e28baa91a14b4a0d9604a8a4eda000
Dawid Paszko <dawid.paszko@gmail.com>
2023-04-13 18:29:02 +0200

    missing keys in ProductForm.js

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -96,11 +96,11 @@ export default function ProductForm({
                 onChange={ev => setCategory(ev.target.value)}>
           <option value="">Uncategorized</option>
           {categories.length > 0 && categories.map(c => (
-            <option value={c._id}>{c.name}</option>
+            <option key={c._id} value={c._id}>{c.name}</option>
           ))}
         </select>
         {propertiesToFill.length > 0 && propertiesToFill.map(p => (
-          <div className="">
+          <div key={p.name} className="">
             <label>{p.name[0].toUpperCase()+p.name.substring(1)}</label>
             <div>
               <select value={productProperties[p.name]}
@@ -109,7 +109,7 @@ export default function ProductForm({
                       }
               >
                 {p.values.map(v => (
-                  <option value={v}>{v}</option>
+                  <option key={v} value={v}>{v}</option>
                 ))}
               </select>
             </div>
