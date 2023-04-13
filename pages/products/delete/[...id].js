7516d9def303ccbeaba26b78110a9ed71988317c
dejwid <dawid.paszko@gmail.com>
2023-04-13 18:33:55 +0200

    escape quotes

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -25,7 +25,7 @@ export default function DeleteProductPage() {
   return (
     <Layout>
       <h1 className="text-center">Do you really want to delete
-        &nbsp;"{productInfo?.title}"?
+        &nbsp;&quot;{productInfo?.title}&quot;?
       </h1>
       <div className="flex gap-2 justify-center">
         <button
